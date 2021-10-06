b39155b06ad219c8dcc951f21327eed37c6488ce
Jennifer Susan <jennifersusan@Jennifers-MBP.cable.rcn.com>
2021-10-06 13:19:54 -0500

    set up index route

--- controllers/wins.js
+++ controllers/wins.js
@@ -3,3 +3,12 @@ const router = express.Router();
 const Win = require('../models/win');
 
 module.exports = router
+
+//index
+router.get('/', (req, res) => {
+    Win.find({}, (error, allWins) => {
+    res.render('index.ejs', {
+        wins: allWins
+    })
+})
+})
